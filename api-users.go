bd030fcf43469ed83cd13ae9ea30897bde6b1e51
syrvann <dragonsparks2050@gmail.com>
2023-03-10 15:45:08 +0100

    Ajout de tous les endpoints Contenus des endpoints à modifier pour relier avec la base SQL

--- api-users.go
+++ api-users.go
@@ -1,22 +1,121 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	fmt.Println("Test API base")
+	// Initialisation de l'API
 
 	r := gin.Default()
-	r.GET("/users", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Page des utilisateurs",
-		})
-	})
+
+	r.GET("/users", usersPage)
+	r.POST("/users", usersPost)
+	r.PUT("/users", usersPut)
+	r.DELETE("/users", usersDelete)
+
+	r.GET("/roles", rolesPage)
+	r.POST("/roles", rolesPost)
+	r.PUT("/roles", rolesPut)
+	r.DELETE("/roles", rolesDelete)
+
+	r.GET("/groups", groupsPage)
+	r.POST("/groups", groupsPost)
+	r.PUT("/groups", groupsPut)
+	r.DELETE("/groups", groupsDelete)
+
+	r.POST("/auth", authJWT)
 
 	r.Run()
 
+}
+
+// Fonctions pour le contenu des pages users
+
+func usersPage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des utilisateurs GET",
+	})
+}
+
+func usersPost(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des utilisateurs POST",
+	})
+}
+
+func usersPut(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des utilisateurs PUT",
+	})
+}
+
+func usersDelete(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des utilisateurs DELETE",
+	})
+}
+
+// Fonctions pour le contenu des pages roles
+
+func rolesPage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des roles GET",
+	})
+}
+
+func rolesPost(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des roles POST",
+	})
+}
+
+func rolesPut(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des roles PUT",
+	})
+}
+
+func rolesDelete(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des roles DELETE",
+	})
+}
+
+// Fonctions pour le contenu des pages groups
+
+func groupsPage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des groups GET",
+	})
+}
+
+func groupsPost(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des groups POST",
+	})
+}
+
+func groupsPut(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des groups PUT",
+	})
+}
+
+func groupsDelete(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Page des groups DELETE",
+	})
+}
+
+// Fonction pour auth
+
+func authJWT(c *gin.Context) {
+	yourHtmlString := "AJOUT AUTHENTIFICATION"
+	c.Writer.WriteHeader(http.StatusOK)
+	c.Writer.Write([]byte(yourHtmlString))
+	return
 }
