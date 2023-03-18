00885e8841c2bbea8e03a5c47d2acb757d85c453
syrvann <dragonsparks2050@gmail.com>
2023-03-18 10:23:31 +0100

    suppression des dossiers

--- main.go
+++ main.go
@@ -4,7 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
+
+
+type User struct {
+	gorm.Model
+	ID        uint    `gorm:"primary_key" json:"id"`
+	Username  string  `json:"name"`
+	Email     string  `json:"email"`
+	Password  string  `json:"password"`
+	Roles     []Role  `gorm:"many2many:user_roles;" json:"roles"`
+	Groups    []Group `gorm:"many2many:user_groups;" json:"groups"`
+	CreatedAt int64   `json:"created_at"`
+	UpdatedAt int64   `json:"updated_at"`
+	DeletedAt int64   `json:"deleted_at"`
+}
+
 
 type Role struct {
 	ID          uint   `gorm:"primary_key" json:"id"`
@@ -27,8 +43,17 @@ type Group struct {
 
 // Connexion à la DB
 
-func load() {
-	init.connectDB()
+var db *gorm.DB
+
+func connectDB() {
+	var err error
+
+	db, err = gorm.Open("postgres", "host=localhost user=myuser dbname=mydb sslmode=disable password=mypassword")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
 }
 
 func main() {
