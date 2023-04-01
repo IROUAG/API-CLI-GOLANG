72c5ce63c38ee9381d0c4826da6627c4232bf703
IROUAG <ilies.rouag28@gmail.com>
2023-04-01 23:19:17 +0200

    error fix

--- cli/main.go
+++ cli/main.go
@@ -14,6 +14,33 @@ func main() {
 		Use:   "your-cli",
 		Short: "CLI for interacting with the API",
 		Long:  `CLI for interacting with the API. Supports login, refresh, logout, and user management.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			// Read user input in a loop
+			reader := bufio.NewReader(os.Stdin)
+			for {
+				fmt.Print("> ")
+
+				// Read the user's input
+				input, err := reader.ReadString('\n')
+				if err != nil {
+					fmt.Println("Error reading input:", err)
+					continue
+				}
+
+				// Remove newline characters
+				input = strings.TrimSuffix(input, "\n")
+				input = strings.TrimSuffix(input, "\r")
+
+				// Split input into arguments
+				args := strings.Fields(input)
+
+				// Execute the command with the provided arguments
+				cmd.SetArgs(args)
+				if err := cmd.Execute(); err != nil {
+					fmt.Println("Error executing command:", err)
+				}
+			}
+		},
 	}
 
 	loginCmd := createLoginCmd()
@@ -28,30 +55,5 @@ func main() {
 
 	if err := rootCmd.Execute(); err != nil {
 		panic(err)
-	}
-	// Read user input in a loop
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("> ")
-
-		// Read the user's input
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading input:", err)
-			continue
-		}
-
-		// Remove newline characters
-		input = strings.TrimSuffix(input, "\n")
-		input = strings.TrimSuffix(input, "\r")
-
-		// Split input into arguments
-		args := strings.Fields(input)
-
-		// Execute the command with the provided arguments
-		rootCmd.SetArgs(args)
-		if err := rootCmd.Execute(); err != nil {
-			fmt.Println("Error executing command:", err)
-		}
 	}
 }
